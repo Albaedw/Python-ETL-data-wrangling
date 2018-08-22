ea60068e3779bcb8e0c5f6344b1c975b0825ec96
Albaedw <albertsjob@gmail.com>
2018-08-22 00:03:49 -0700

    store nested dictionary structure example

---
+++ nested_dict.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+cars = {'Car': { 'type': { 'suv'        : {'color': ['red','yellow','black']
+                                          },
+                           'truck' : {'big': { 'color' : ['red','yellow'],
+                                               'weight': ['heavy']
+                                                    },
+                                      'small': {'color' : ['black','red'],
+                                                'weight': ['light'],
+                                                'length': ['short','long']
+                                               }
+                                      }
+                          }
+                      },
+               'exterior' : {'color' : ['green','red']}
+             }
+
+
+def filterNestedDict(node, searchItem):
+    if isinstance(node,list):
+       for x in node:
+          if x == searchItem:
+           return node
+       return None
+    else:
+        f_node={}
+        for key, val in node.iteritems():
+            cur_node = filterNestedDict(val, searchItem)
+            if cur_node:
+              f_node[key] = cur_node
+        return f_node or None
+
+res=filterNestedDict(cars, "red")
+print res
